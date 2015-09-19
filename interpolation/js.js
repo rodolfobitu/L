199e9664163d5b646d0450f89a7e706284c0696f
Guilherme Souza <sitegui@sitegui.com.br>
2015-09-19 17:40:08 -0300

    Adionado interação de arrastar os pontos

--- interpolation/js.js
+++ interpolation/js.js
@@ -21,6 +21,18 @@ let width = 0
 /** @var {number} */
 let height = 0
 
+/**
+ * `null` if no point is being dragged, point
+ * position (`i`) otherwise
+ * @var {?number}
+ */
+let dragging = null
+
+/**
+ * @var {boolean}
+ */
+let waitingDraw = false
+
 window.addEventListener('resize', resizeCanvas)
 window.addEventListener('load', init)
 
@@ -31,7 +43,9 @@ function init() {
 	canvas = document.getElementById('canvas')
 	context = canvas.getContext('2d')
 	resizeCanvas()
-	draw()
+	canvas.addEventListener('mousedown', handleMouseDown)
+	canvas.addEventListener('mousemove', handleMouseMove)
+	canvas.addEventListener('mouseup', handleMouseUp)
 }
 
 /**
@@ -40,13 +54,15 @@ function init() {
 function resizeCanvas() {
 	width = canvas.width = canvas.clientWidth
 	height = canvas.height = canvas.clientHeight
-	draw()
+	requestDraw()
 }
 
 /**
  * Redraw the canvas content
  */
 function draw() {
+	waitingDraw = false
+
 	// Reset
 	context.clearRect(0, 0, width, height)
 
@@ -55,6 +71,7 @@ function draw() {
 		let {
 			x, y
 		} = toCanvasXY(i)
+		context.beginPath()
 		context.arc(x, y, 5, 0, 2 * Math.PI)
 		context.fill()
 	})
@@ -69,5 +86,68 @@ function toCanvasXY(i) {
 	return {
 		x: width / (numPoints + 1) * (i + 1),
 		y: height * (0.1 + 0.8 * points[i] / 100)
+	}
+}
+
+/**
+ * Convert from canvas space to point space
+ * @param {number} x
+ * @param {number} y
+ * @returns {{i: number, isNear: boolean, value: number}}
+ */
+function fromCanvasXY(x, y) {
+	let value = 100 * (y / height - 0.1) / 0.8,
+		rawI = x / width * (numPoints + 1) - 1,
+		nearestI = Math.min(Math.max(0, Math.round(rawI)), numPoints - 1)
+	return {
+		i: nearestI,
+		isNear: Math.abs(nearestI - rawI) < 0.1,
+		value: Math.min(Math.max(0, value), 100)
+	}
+}
+
+/**
+ * @param {MouseEvent} event
+ */
+function handleMouseDown(event) {
+	let {
+		i, isNear
+	} = fromCanvasXY(event.clientX, event.clientY)
+
+	if (!isNear) {
+		return
+	}
+
+	dragging = i
+	handleMouseMove(event)
+}
+
+/**
+ * @param {MouseEvent} event
+ */
+function handleMouseMove(event) {
+	if (dragging === null) {
+		return
+	}
+
+	points[dragging] = fromCanvasXY(event.clientX, event.clientY).value
+	requestDraw()
+}
+
+/**
+ * @param {MouseEvent} event
+ */
+function handleMouseUp(event) {
+	handleMouseMove(event)
+	dragging = null
+}
+
+/**
+ * Schedule the drawing
+ */
+function requestDraw() {
+	if (!waitingDraw) {
+		waitingDraw = true
+		window.requestAnimationFrame(draw)
 	}
 }
