aa4fc6d249aeba8dcf560b6b895a02f821deabdc
Guilherme Souza <sitegui@sitegui.com.br>
2015-10-22 01:20:59 -0200

    Corrija a implementação dos delays
    Usando as macros do compilador com base no número de instruções

--- headers/util.h
+++ headers/util.h
@@ -1,10 +1,13 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-void util_genDelay1MS(void);
+/* Wait a multiple of 1ms, up to 10ms */
+void util_genDelay1MS(char cN);
 
+/* Wait a multiple of 10ms, up to 100ms */
 void util_genDelay10MS(char cN);
 
+/* Wait a multiple of 100ms, up to 1000ms */
 void util_genDelay100MS(char cN);
 
 #endif /* UTIL_H */
--- src/util.c
+++ src/util.c
@@ -1,34 +1,22 @@
 #include "util.h"
 #include "L.h"
 
-void util_genDelay1MS(void) {
-	int i;
-	for (i=0; i<1000; i++) {
-		Nop();
-		Nop();
-		Nop();
-		Nop();
-		Nop();
-		Nop();
-		Nop();
-		Nop();
-		Nop();
-		Nop();
-		Nop();
-		Nop();
-	}
+extern void Delay1KTCYx(unsigned char unit);
+extern void Delay10KTCYx(unsigned char unit);
+
+void util_genDelay1MS(char cN) {
+	/* Fcy = 12MHz => 1ms = 12kcy */
+	Delay1KTCYx(12 * cN);
 }
 
 void util_genDelay10MS(char cN) {
-	int i, cMax = 10*cN;
-	for(i = 0; i < cMax; i++) {
-		util_genDelay1MS();
-	}
+	/* Fcy = 12MHz => 10ms = 120kcy */
+	Delay10KTCYx(12 * cN);
 }
 
 void util_genDelay100MS(char cN) {
-	int i, cMax = 10*cN;
-	for(i = 0; i < cMax; i++) {
-		util_genDelay10MS(1);
+	char cI;
+	for (cI = 0; cI < cN; cI++) {
+		util_genDelay10MS(10);
 	}
 }
